c2beeca794f484c89faf82eb83fddc57f3d0972a
ssad-80y <51715961+ssad-80y@users.noreply.github.com>
2019-08-01 09:28:55 +0300

    Update pix2pix.py

--- pix2pix.py
+++ pix2pix.py
@@ -25,7 +25,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--epoch", type=int, default=0, help="epoch to start training from")
 parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
 parser.add_argument("--dataset_name", type=str, default="facades", help="name of the dataset")
-parser.add_argument("--batch_size", type=int, default=8, help="size of the batches")
+parser.add_argument("--batch_size", type=int, default=1, help="size of the batches")
 parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
 parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
 parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
@@ -35,7 +35,7 @@ parser.add_argument("--img_height", type=int, default=256, help="size of image h
 parser.add_argument("--img_width", type=int, default=256, help="size of image width")
 parser.add_argument("--channels", type=int, default=3, help="number of image channels")
 parser.add_argument(
-    "--sample_interval", type=int, default=320, help="interval between sampling of images from generators"
+    "--sample_interval", type=int, default=50, help="interval between sampling of images from generators"
 )
 parser.add_argument("--checkpoint_interval", type=int, default=50, help="interval between model checkpoints")
 opt = parser.parse_args()
